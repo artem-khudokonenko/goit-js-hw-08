291f51f0a3157e23c6da0678f1031d5ed1c53f83
artem-khudokonenko <aftinid94@gmail.com>
2023-05-24 19:27:16 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const submitEl=formEl.querySelector('button')
-const emailEl = formEl.elements.email;
-const textEl = formEl.elements.message;
+const emailEl = formEl.email;
+const textEl = formEl.message;
 formEl.addEventListener('input', throttle(calbackInput,500));
 function calbackInput(e) {
   const emailText = emailEl.value;
@@ -13,16 +13,21 @@ function calbackInput(e) {
   );
 }
 window.addEventListener('load', () => {
-  if ((localStorage.length=1)) {
-    emailEl.value = '';
-    textEl.value = '';
-  }else{
   const emailLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
-  emailEl.value = emailLocal.email;
-  textEl.value = emailLocal.message;
-}});
-submitEl.addEventListener("click",()=>{
-    const a=localStorage.getItem("feedback-form-state")
-    console.log(a)
+  if (emailLocal) {
+    emailEl.value = emailLocal.email || "";
+    textEl.value = emailLocal.message || "";
+
+  }});
+submitEl.addEventListener("click",(e)=>{
+  e.preventDefault()
+  if (emailEl.value &&textEl.value) {
+    
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")))
+    formEl.reset()
     localStorage.removeItem("feedback-form-state")
+    
+  }else{
+    return alert("Fill in all")
+  }
 })
